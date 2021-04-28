7a5999bd0436e501a84e7cd3d24b779763b304c2
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2021-04-28 09:51:24 +0700

    id + role login google

--- controller/googleLoginController.js
+++ controller/googleLoginController.js
@@ -12,9 +12,11 @@ class Controller{
     }
 
     static callbackGoogle(req, res) {
+        let id = req.user[0].dataValues.id
+        let role = req.user[0].dataValues.role
         let token = generateToken(req.user[0].dataValues)
         // res.redirect('http://namaweb.org/sukses?token='+token);   
-         res.redirect('http://localhost:8080/sukses?token='+token); 
+         res.redirect('http://localhost:8080/sukses?token='+token+'&'+id+'&'+role); 
   }
   static authFailed (req, res) {
     req.json({ message:"anda gagal login"})
