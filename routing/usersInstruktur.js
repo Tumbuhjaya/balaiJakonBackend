f1dd233e7b4f8dab858ea8e82686436fbcdb7c11
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2021-06-14 15:08:55 +0700

    upload katepe

--- routing/usersInstruktur.js
+++ routing/usersInstruktur.js
@@ -1,12 +1,12 @@
 const controller = require('../controller/usersInstrukturController')
 const router = require('express').Router()
 const authentification=require('../middleware/authentification')
-
+const upload = require('../helper/upload')
 
 router.post('/register',controller.register)
 router.post('/update',authentification,controller.update)
 router.get('/profile',authentification,controller.profile)
-router.post('/uploadKTP',authentification,controller.uploadKTP)
+router.post('/uploadKTP',upload,authentification,controller.uploadKTP)
 router.get('/allByRole/:role',authentification,controller.allByRole)
 
 
--- routing/usersSiswa.js
+++ routing/usersSiswa.js
@@ -1,12 +1,12 @@
 const controller = require('../controller/usersSiswaController')
 const router = require('express').Router()
 const authentification=require('../middleware/authentification')
-
+const upload = require('../helper/upload')
 
 router.post('/register',controller.register)
 router.post('/update',authentification,controller.update)
 router.get('/profile',authentification,controller.profile)
-router.post('/uploadKTP',authentification,controller.uploadKTP)
+router.post('/uploadKTP',authentification,upload,controller.uploadKTP)
 
 
 module.exports=router
