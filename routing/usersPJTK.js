7ee6c9e8a8defe52bcabf9c6fe9a2f75cd2231b2
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2021-06-14 15:05:35 +0700

    uploadKTP

--- routing/usersPJTK.js
+++ routing/usersPJTK.js
@@ -1,11 +1,11 @@
 const controller = require('../controller/usersPJTKController')
 const router = require('express').Router()
 const authentification=require('../middleware/authentification')
-
+const upload = require('../helper/upload')
 
 router.post('/register',controller.register)
 router.post('/update',authentification,controller.update)
 router.get('/profile',authentification,controller.profile)
-router.post('/uploadKTP',authentification,controller.uploadKTP)
+router.post('/uploadKTP',authentification,upload,controller.uploadKTP)
 
 module.exports=router
